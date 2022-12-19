fe5dde8e138049a9804abaa9cf89fb905d2e283f
William <will@claritycyber.com>
2022-12-19 13:14:44 -0500

    Part 1 Done

--- main.py
+++ main.py
@@ -3,15 +3,36 @@ with open('input.txt', 'r') as f:
     datastream = [entry.strip() for entry in lines]
 
 
+def has_repeated_letters(s):
+    # Create a dictionary to store the count of each letter
+    letter_counts = {}
 
-def subroutine(list):
-    for i in range(0, len(list) - 3):
-        potential = list[i:i+4]
+    # Iterate through each letter in the string
+    for letter in s:
+        # If the letter is not in the dictionary, add it with a count of 1
+        if letter not in letter_counts:
+            letter_counts[letter] = 1
+        # If the letter is already in the dictionary, increase its count by 1
+        else:
+            letter_counts[letter] += 1
 
-        for letter in potential:
-            if potential.rfind(letter) > -1:
-                break
-            else:
-                return i+3
+    # Iterate through the dictionary and check if any letters have a count greater than 1
+    for letter, count in letter_counts.items():
+        if count > 1:
+            return True
 
-print(subroutine(datastream))
+    # If no letters have a count greater than 1, return False
+    return False
+
+
+def subroutine(data):
+    retIndex = None
+    for i in range(0, len(data) - 3):
+        potential = data[i:i + 4]
+        print(potential)
+        if not has_repeated_letters(potential):
+            return i+4
+
+
+
+print(subroutine(datastream[0]))
