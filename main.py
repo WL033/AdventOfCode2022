f261d15b0766994ab0e159cb677c834986969641
William <will@claritycyber.com>
2022-12-19 11:51:05 -0500

    still working

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 with open('input.txt', 'r') as f:
     lines = f.readlines()
-    input = [entry.strip() for entry in lines]
+    datastream = [entry.strip() for entry in lines]
+
+
+
+def subroutine(list):
+    for i in range(0, len(list) - 3):
+        potential = list[i:i+4]
+
+        for letter in potential:
+            if potential.rfind(letter) > -1:
+                break
+            else:
+                return i+3
+
+print(subroutine(datastream))
