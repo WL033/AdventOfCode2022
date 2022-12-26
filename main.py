9daf8ed5e0bf57e5a80fc44c47851f13514c2f58
William <will@claritycyber.com>
2022-12-26 17:30:23 -0500

    Part 1 Redone Final (Actually works now)

--- main.py
+++ main.py
@@ -1,53 +1,45 @@
+import path as path
+
 with open('input.txt', 'r') as f:
     lines = f.readlines()
+    outputs = [entry.strip() for entry in lines]
 
-cdIndexes = []
-for i in range(0, len(lines)):
-    if lines[i].find("$ cd") > -1 and lines[i].find("..") == -1:
-        cdIndexes.append(i)
-cdIndexes.append(len(lines))
-cdStrips = []
-for i in range(0, len(cdIndexes) - 1):
-    cdStrip = []
-    for j in range(cdIndexes[i], cdIndexes[i + 1]):
-        if lines[j].find("$ cd ..") == -1 and lines[j].find("$ ls") == -1:
-            cdStrip.append(lines[j].strip())
-    cdStrips.append(cdStrip)
+currentPath = "/root"
+directories = {"/root": 0}
 
-processedDirectories = []
-for strip in cdStrips:
-    dirName = strip[0][5:]
-    directories = []
-    files = []
-    fileSizes = []
-    for element in strip:
-        if element.find("dir ") > -1:
-            directories.append(element[4:])
-        elif element[0].isdigit():
-            lastCharIndex = None
-            for c in range(0, len(element)):
-                if element[c].isdigit():
-                    lastCharIndex = c
-            fileSizes.append(int(element[0:(lastCharIndex + 1)]))
-            files.append(dict(name=element[(lastCharIndex + 2):], size=int(element[0:(lastCharIndex + 1)])))
-    processedDirectories.append(dict(name=dirName, size=sum(fileSizes), d=directories, f=files))
+for output in outputs:
 
-fileNames = []
-for directory in processedDirectories:
-    fileNames.append(directory["name"])
+    if output.find("$") != -1:  # $
 
-sizeDictionary = []
-for directory in processedDirectories:
-    subDirSum = 0
-    if(len(directory["d"]) > 0):
-        for ref in directory["d"]:
-            subDirSum += processedDirectories[fileNames.index(ref)]["size"]
-    sizeDictionary.append(dict(name=directory["name"], size=directory["size"] + subDirSum))
+        if output.find("cd") != -1:  # $cd
+
+            if output.find("..") != -1:
+
+                currentPath = currentPath[0:currentPath.rfind("/")]
+
+            elif output.find("/") != -1:
+                currentPath = "/root"
+
+            else:  # $ cd [dirName]
+                currentPath = currentPath + "/" + output[output.find("cd") + 3:]
+                directories.update({currentPath: 0})
+
+    else:  # directory or file
+        if output[0].isdigit():
+            size = int(output[0:output.find(" ")])
+
+            parsingPath = currentPath
+            for i in range(0, currentPath.count("/")):
+                directories[parsingPath] += size
+                parsingPath = parsingPath[0:parsingPath.rfind("/")]
+
+
 
 deletionSize = 0
-for element in sizeDictionary:
-    if element["size"] < 100000:
-        deletionSize += element["size"]
+
+for directory in directories:
+    print(directory, directories[directory])
+    if directories[directory] <= 100000:
+        deletionSize += directories[directory]
 
 print(deletionSize)
-
